60a98d9d7f6864dbe4f64c0b9ec0b2577939b598
dangtran3110 <104581286+dangtran3110@users.noreply.github.com>
2022-08-23 21:54:20 +0700

    filereg.php

---
+++ filereg.php
@@ -0,0 +1,87 @@
+<?php
+system("clear");
+echo (" \033[1;92m
+══════════════════════════════════════════════════════════
+Dev:Trần Hải Đăng.                        FB: Hải Đăng 
+                                                 
+            /\              𝑯𝑨𝑰              /\
+              –-------------------------------–
+            \/               𝑫𝑨𝑵𝑮            \/
+            
+            
+HOTLINE: 0901349948.                   YT: Trunks Gaming
+══════════════════════════════════════════════════════════\n");
+echo ("\033[1;96m Day La Regedit Mobile Duoc Lam Boi Tran Hai Dang");
+echo ("\n");
+echo "\033[1;93m Password:   ";
+$pass = trim(fgets(STDIN));
+if($pass == "HaiDang"){
+	echo "\033[1;92m Password√\n";
+    echo "\033[1;93m Day La Reg Khong Phai Hack Hay Cheat Nen Ko Ban Acc 100%\n";
+}else{
+echo "\033[1;91m Password×";
+exit;
+}
+echo "\033[1;96m Bạn có chắc đã mua bên Trần Hải Đăng\n";
+echo "\033[1;92m Y/N\n";
+$pass = trim(fgets(STDIN));
+ if($pass == "Y"){
+echo "Đang Gửi Lệnh Yều Cầu Cấp Quyền Truy Cập File\n";
+}else{
+echo "\033[1;91m Không Hoàn Thành";
+exit;
+}
+echo ("Đang Gửi Lệnh Vui Lòng Chờ 5 Phút Để tiến Hành Xác Nhận");
+echo ("\n");
+sleep (5);
+echo ("Đang Gửi Lệnh Vui Lòng Chờ 5 Phút Để tiến Hành Xác Nhận");
+echo ("\n");
+sleep (5);
+echo ("Đang Gửi Lệnh Vui Lòng Chờ 5 Phút Để tiến Hành Xác Nhận");
+echo ("\n");
+sleep (5);
+echo ("Đang Gửi Lệnh Vui Lòng Chờ 5 Phút Để tiến Hành Xác Nhận");
+echo ("\n");
+sleep (5);
+echo ("Đang Gửi Lệnh Vui Lòng Chờ 5 Phút Để tiến Hành Xác Nhận");
+echo ("\n");
+sleep (5);
+echo ("Đang Gửi Lệnh Vui Lòng Chờ 5 Phút Để tiến Hành Xác Nhận");
+echo ("\n");
+sleep (5);
+echo ("Đang Gửi Lệnh Vui Lòng Chờ 5 Phút Để tiến Hành Xác Nhận");
+echo ("\n");
+sleep (5);
+echo ("Đã xác nhận cài file Reg Limited 5 User Thành Công\n");
+sleep (5);
+echo "\033[1;96m Bạn xác nhận sử dụng file Reg Limited 5 User\n";
+echo "\033[1;92m Y/N\n";
+$pass = trim(fgets(STDIN));
+ if($pass == "Y"){
+echo "File Đang Bắt Đầu Chạy\n";
+}else{
+echo "\033[1;91m Không Hoàn Thành";
+exit;
+}
+echo ("\033[1;95m Reg Đã Được Bật\n");
+sleep (1);
+echo ("\033[1;95m Reg sẽ bắt đầu chạy sau 5\n");
+sleep (1);
+echo ("\033[1;95m Reg sẽ bắt đầu chạy sau 4\n");
+sleep (1);
+echo ("\033[1;95m Reg sẽ bắt đầu chạy sau 3\n");
+sleep (1);
+echo ("\033[1;95m Reg sẽ bắt đầu chạy sau 2\n");
+sleep (1);
+echo ("\033[1;95m Reg sẽ bắt đầu chạy sau 1\n");
+sleep (1);
+echo ("\033[1;95m Reg sẽ bắt đầu chạy sau 0\n");
+sleep (1);
+echo ("\033[1;95m Reg Đang Tiến Hành Chạy \n");
+sleep (1);
+while(true){
+echo ("\033[1;92m Reg Đang Chạy ");
+echo ("\n");
+sleep (1);
+}
+?>
